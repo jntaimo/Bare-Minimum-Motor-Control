4cc0c00c87f74959a524fc3209483ce000d5be81
jntaimo <jntaimo@gmail.com>
2024-01-25 11:14:29 -0500

    Fixed CPR and removed Tau

--- src/test_code/encoder_test.cpp
+++ src/test_code/encoder_test.cpp
@@ -2,15 +2,14 @@
 #include "pinout.h"
 
 
-#define COUNTS_PER_REVOLUTION 103.8
+#define COUNTS_PER_REVOLUTION 537.7 //312 RPM
 
 #define PRINT_DELAY 30 // Delay between printing to serial in milliseconds
-#define TAU 0.01 // Time constant for velocity filter in seconds
 static unsigned long lastPrintTime = 0;
 
 // Encoder velocity readers
-EncoderVelocity leftEncoder(LEFT_ENCODER_A_PIN, LEFT_ENCODER_B_PIN, COUNTS_PER_REVOLUTION, TAU);
-EncoderVelocity rightEncoder(RIGHT_ENCODER_A_PIN, RIGHT_ENCODER_B_PIN, COUNTS_PER_REVOLUTION, TAU);
+EncoderVelocity leftEncoder(LEFT_ENCODER_A_PIN, LEFT_ENCODER_B_PIN, COUNTS_PER_REVOLUTION);
+EncoderVelocity rightEncoder(RIGHT_ENCODER_A_PIN, RIGHT_ENCODER_B_PIN, COUNTS_PER_REVOLUTION);
 
 void setup() {
     Serial.begin(115200);
