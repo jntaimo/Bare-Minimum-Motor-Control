a7468125aa346a255120231cffa0b97390b5973e
jntaimo <jntaimo@gmail.com>
2024-01-29 15:16:00 -0500

    Added separate RPMS for different motors

--- include/drive_single.h
+++ include/drive_single.h
@@ -1,5 +1,10 @@
 #ifndef DRIVE_H
 #define DRIVE_H
+//gobilda motor counts
+//https://www.gobilda.com/yellow-jacket-planetary-gear-motors/
+#define CPR_30_RPM 5281.1
+#define CPR_60_RPM 2786.2
+#define CPR_312_RPM 537.7
 
 #define PWM_FREQ 25000
 //max number of bits for pwm
