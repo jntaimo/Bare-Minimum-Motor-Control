cf13f919b9f633b07c38a4944eca70e827969090
Joseph Ntaimo <jntaimo@gmail.com>
2024-02-01 21:20:21 -0500

    Made encoder single

--- src/test_code/encoder_basic_test.cpp
+++ src/test_code/encoder_basic_test.cpp
@@ -3,7 +3,6 @@
 #include "pinout.h"
 
 ESP32Encoder encoder;
-ESP32Encoder encoder2;
 
 
 void setup(){
@@ -14,11 +13,10 @@ void setup(){
 	ESP32Encoder::useInternalWeakPullResistors=UP;
 
 	encoder.attachFullQuad(LEFT_ENCODER_A_PIN, LEFT_ENCODER_B_PIN);
-	encoder2.attachFullQuad(RIGHT_ENCODER_A_PIN, RIGHT_ENCODER_B_PIN);
 }
 
 void loop(){
 	// Loop and read the count
-	Serial.println("Encoder count = " + String((int32_t)encoder.getCount()) + " " + String((int32_t)encoder2.getCount()));
+	Serial.println("Encoder count = " + String((int32_t)encoder.getCount()));
 	delay(100);
 }
